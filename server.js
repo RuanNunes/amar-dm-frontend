dd1adc78f49fc1561c74ec21b09f392c58ebda21
Ruan Nunes <ruanschwanz@gmail.com>
2020-03-29 21:05:21 -0300

    ajust server from heroku

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http'); //importing http
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+
+
+
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'amar-dm.herokuapp.com'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU PING SUCCESS");
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
